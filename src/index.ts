553355157d08c4bb043d76e86274cacbaeaa896f
austin-mccollom <52938752+austin-mccollom@users.noreply.github.com>
2025-10-27 17:41:09 -0400

    Update index.ts with instructions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
+/**
+ * Code signal: banking system
+ * 
+ * Requirements
+ * Your task is to implement a simplified version of a banking system.
+ * 
+ * Level 1: The banking system should support creating new accounts and depositing money into 
+ * and withdrawing/paying money from accounts.
+ * 
+ * Level 2: The banking system should support ranking accounts based on the total value of transactions.
+ * 
+ * Level 3: The banking system should support scheduling transfers and checking the scheduled transfer status.
+ * 
+ * Level 4: The banking system should support merging two accounts while retaining the balances 
+ * and transaction histories of the original accounts.
+ * 
+ * To move to the next level, you should pass all the tests at the current level.
+ */
+
 class Transaction {
     private id: number;
     private timestamp: number; // execute at for scheduled txs.
